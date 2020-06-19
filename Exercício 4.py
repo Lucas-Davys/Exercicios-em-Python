e332c75e81eeee7556abc14d8980a7c512d829df
Lucas-Davys <65188266+Lucas-Davys@users.noreply.github.com>
2020-06-19 17:54:09 -0300

    Delete Exercício 4.py

--- Exercício 4.py
+++
@@ -1,5 +0,0 @@
-
-medida_metro = int(input('Informe a medida em metros: '))
-medida_centimetro = medida_metro * 100
-
-print('\n {} metro(s) equivale(m) a {} centímetro(s)'.format(medida_metro,medida_centimetro))
