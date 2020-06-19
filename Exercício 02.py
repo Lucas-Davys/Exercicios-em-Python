c25568f8fd77b3258ad12fb9f82900334de7e880
Lucas-Davys <65188266+Lucas-Davys@users.noreply.github.com>
2020-06-19 17:58:25 -0300

    Delete Exercício 02.py

--- Exercício 02.py
+++
@@ -1,6 +0,0 @@
-
-numero_1 = int(input('Informe o primeiro número: '))
-numero_2 = int(input('Informe o segundo número: '))
-soma = numero_1 + numero_2
-
-print('A soma dos números informados é: {}'.format(soma))
